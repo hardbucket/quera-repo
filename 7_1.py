e158618af1259250fa69a3d4e6530b4d615c11e9
hardbucket <silentochaos@gmail.com>
2022-09-19 20:26:51 +0430

    adding file 7_1

---
+++ 7_1.py
@@ -0,0 +1,14 @@
+weight = int(input())
+height = float(input())
+
+BMI = weight/(height * height)
+print(f"{BMI:.2f}")
+
+if BMI < 18.5:
+    print("Underweight")
+if 18.5 <= BMI < 25:
+    print("Normal")
+if 25 <= BMI < 30:
+    print("Overweight")
+if 30 <= BMI:
+    print("Obese")
